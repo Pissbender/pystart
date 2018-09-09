e0759b2807762be36bdb2fa94dcb91c67058fa28
pissbender <pissbender@nigge.rs>
2018-09-09 13:04:47 +0100

    Chapter One: Avoid Using start, end and stride in a single slice

--- First Chapter/slice_sequence.py
+++ First Chapter/slice_sequence.py
@@ -4,7 +4,7 @@ print("First four: ", a[:4])
 print("Last four: ", a[-4:])
 print("Middle two: ", a[3:-3])
 
-print("First Twenty: ", a[:20])
+print("\nFirst Twenty: ", a[:20])
 print("Last Twenty: ", a[-20:])
 
 b = a[4:]
@@ -16,3 +16,23 @@ print("No change: ", a)
 print("\nBefore: ", a)
 a[2:7] = [99, 22, 14]
 print("After: ", a)
+
+a = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+odds = a[::2]
+evens = a[1::2]
+print("\nOdds: ", odds)
+print("Evens: ", evens)
+
+x = b'mongoose'
+y = x[::-1]
+print("\nY: ", y)
+
+a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+print("\n")
+print(a[::2])
+print(a[::-2])
+print("\n")
+print(a[2::2])
+print(a[-2::-2])
+print(a[-2:2:-2])
+print(a[2:2:-2])
